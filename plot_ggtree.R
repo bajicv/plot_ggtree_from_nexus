d9c8ef6fa370e4f6e772139fb48244a6a7914639
Vladimir Bajić <44064386+bajicv@users.noreply.github.com>
2024-02-15 16:56:32 +0100

    added -r option
    -r option allows selecting an outgroup for rooting the tree. It ensures that outgroup will be displayed at the bottom of the plot, and it colors it in red.

--- plot_ggtree.R
+++ plot_ggtree.R
@@ -4,17 +4,26 @@
 # Author:
 #   Vladimir Bajić
 #
-# Date:
-#   January 2024
+# Last update:
+#   15 February 2024
 #
 # Usage:
+#   - Minimal example run requires a nexus file containing mutations
 #   Rscript --vanilla plot_ggtree.R -i input.nexus
 #
+#   - Plot and output in specific location. Do not provide extention.
+#     It automatically saves .jpeg .pdf and .png files.
+#   Rscript --vanilla plot_ggtree.R -i input.nexus -o out_path_without_extension
+#
+#   - Plot with specific outgroup at the bottom of the tree.
+#     sampleID's to be used for rooting should be within quotation marks("sampleID").
+#   Rscript --vanilla plot_ggtree.R -i input.nexus -r "sampleID"
+#
 #   TODO:
-#   - add possibility to choose a root
 #   - adjust colors
 #
 ################################################################################
+
 
 # Packages ---------------------------------------------------------------------
 suppressMessages(library(tidyverse))
@@ -22,6 +31,74 @@ suppressMessages(library(ggtree))
 suppressMessages(library(ape))
 suppressMessages(library(treeio))
 library(optparse)
+
+
+# Define functions -------------------------------------------------------------
+
+## Function to plot ggtree -----------------------------------------------------
+plot_ggtree <- function(tree) {
+    gg_tree <-
+        tree %>%
+        ggtree(layout = "rectangular", ladderize = TRUE) +
+        geom_tiplab(align = TRUE, color = "#081d58") +
+        coord_cartesian(clip = "off") +
+        theme(plot.margin = unit(c(10, (plot_width * 3.5), 10, 5), "mm")) +
+        geom_tippoint(color = "#081d58", size = 4) +
+        geom_label(aes(x = branch, label = mutations_labels), fill = "#a1d99b") +
+        geom_rootedge()
+    return(gg_tree)
+}
+
+## Function to plot rooted tree ------------------------------------------------
+plot_tree_with_outgroup <- function(tree) {
+    ### Choose outgroup
+    tree@phylo <-
+        tree@phylo %>%
+        root(outgroup = opt$r, edgelabel = TRUE)
+
+    ### Making temporary tree
+    gg_tree_tmp <- ggtree(tree, layout = "rectangular", ladderize = FALSE)
+
+    ### Reordering branches so that outgroup is at the very bottom
+    init_taxa_order <- get_taxa_name(tree_view = gg_tree_tmp)
+    outgroup_index <- grep(opt$r, init_taxa_order, fixed = TRUE)
+    final_taxa_order <- append(init_taxa_order[-outgroup_index], init_taxa_order[outgroup_index])
+    rev_final_taxa_order <- rev(final_taxa_order)
+
+    ### rotate nodes so that outgroup is at the bottom
+    tree@phylo <- ape::rotateConstr(tree@phylo, rev_final_taxa_order)
+
+    ### Plot tree with the outgroup at the bottom
+    gg_tree <-
+        tree %>%
+        ggtree(layout = "rectangular", ladderize = TRUE) +
+        geom_tiplab(align = TRUE, color = "#081d58") +
+        geom_tiplab(aes(subset = (label == opt$r)), align = TRUE, color = "#b30000") +
+        coord_cartesian(clip = "off") +
+        theme(plot.margin = unit(c(10, (plot_width * 3.5), 10, 5), "mm")) +
+        geom_tippoint(color = "#081d58", size = 4) +
+        geom_tippoint(aes(subset = (label == opt$r)), color = "#b30000", size = 4) +
+        geom_label(aes(x = branch, label = mutations_labels), fill = "#a1d99b") +
+        geom_point2(aes(subset = (label == opt$r)), shape = 23, size = 5, fill = "#b30000") +
+        geom_rootedge()
+
+    return(gg_tree)
+}
+
+## Function to adjust plot margines to ensure that all the lables fit ----------
+ajdust_plot_margin <- function(iqtree) {
+    ### asigne plot_height and plot_width to global enviroment
+    plot_height <<- 5 + (length(iqtree@phylo$tip.label) * 0.2)
+    plot_width <<- 30 + (max(nchar(iqtree@phylo$tip.label)) * 0.3)
+}
+
+## Function to save plots in different formats ---------------------------------
+save_ggtree <- function(gg_tree) {
+    ggsave(paste(opt$o, ".png", sep = ""), gg_tree, height = plot_height, width = plot_width, limitsize = FALSE)
+    ggsave(paste(opt$o, ".pdf", sep = ""), gg_tree, height = plot_height, width = plot_width, limitsize = FALSE)
+    ggsave(paste(opt$o, ".jpeg", sep = ""), gg_tree, height = plot_height, width = plot_width, limitsize = FALSE)
+}
+
 
 # Making option list -----------------------------------------------------------
 option_list <- list(
@@ -39,7 +116,7 @@ option_list <- list(
     )
 )
 
-# Parsing options
+# Parsing options --------------------------------------------------------------
 opt_parser <- OptionParser(option_list = option_list)
 opt <- parse_args(opt_parser)
 
@@ -47,43 +124,36 @@ opt <- parse_args(opt_parser)
 
 if (is.null(opt$i)) {
     print_help(opt_parser)
-    stop("Input file must be provided.")
+    stop("Input file must be provided.\n")
 }
+
+# Load nexus -------------------------------------------------------------------
+cat("Reading in the tree.\n")
+iqtree <- read.mega(opt$i)
+
+# Make mutation labels to be used in plot --------------------------------------
+cat("Making mutation labels.\n")
+iqtree@data$mutations_labels <- unlist(lapply(iqtree@data$mutations, paste, collapse = ", "))
+
+# Ajust plot margin to ensure that labels fit ----------------------------------
+cat("Adjusting plot margins.\n")
+ajdust_plot_margin(iqtree)
 
 if (is.null(opt$o)) {
     opt$o <- tools::file_path_sans_ext(opt$i)
+    cat("Output not specified.\nOutput will be saved as:", opt$o, ".{jpeg,pdf,png}\n", sep = "")
 }
 
 if (is.null(opt$r)) {
-    cat("Using default root.\n")
+    cat("Plotting unrooted tree.\n")
+    ## Plot and save ggtree without speficying outgroup
+    suppressWarnings({
+        plot_ggtree(iqtree) %>% save_ggtree()
+    })
+} else {
+    cat("Plotting tree with an outgroup:", opt$r, "\n")
+    ## Plot and save ggtree without speficying outgroup
+    suppressWarnings({
+        plot_tree_with_outgroup(iqtree) %>% save_ggtree()
+    })
 }
-
-
-# Load nexus -------------------------------------------------------------------
-iqtree <- read.mega(opt$i)
-
-# make mutation labels to be used in plot
-iqtree@data$mutations_labels <- unlist(lapply(iqtree@data$mutations, paste, collapse = ", "))
-
-
-# Plot tree --------------------------------------------------------------------
-
-#  Adjustment for height and width of the plot to ensure that all the lables fit
-plot_height <- 5 + (length(iqtree@phylo$tip.label) * 0.2)
-plot_width <- 30 + (max(nchar(iqtree@phylo$tip.label)) * 0.3)
-
-# Making tree
-gg_tree <-
-    iqtree %>%
-    ggtree(layout = "rectangular") +
-    geom_tiplab(align = TRUE, color = "darkblue") +
-    coord_cartesian(clip = "off") +
-    theme(plot.margin = unit(c(10, (plot_width * 3.5), 10, 5), "mm")) +
-    geom_tippoint(color = "darkblue", size = 4) +
-    geom_label(aes(x = branch, label = mutations_labels), fill = "lightgreen") +
-    geom_rootedge()
-
-
-# Save plot --------------------------------------------------------------------
-ggsave(paste(opt$o, ".png", sep = ""), gg_tree, height = plot_height, width = plot_width, limitsize = FALSE)
-ggsave(paste(opt$o, ".pdf", sep = ""), gg_tree, height = plot_height, width = plot_width, limitsize = FALSE)
